cfb2f4fe6848c129edeccd6ebf65187001711e71
Liz Dahlstrom <liz.dahlstrom@gmail.com>
2016-04-27 09:02:13 +0200

    Added factory, added first progress on test html
    Added factory for json data loading and linked load function as resolve.
    
    Messed around in testCtrl...
    Moved testInfo as ng if div inside test.html

--- controllers/testControllers.js
+++ controllers/testControllers.js
@@ -1,11 +1,45 @@
 var testControllers = angular.module('testControllers', []);
 
-testControllers.controller('userCtrl',['$scope','$http' ,function ($scope , $http) {
-    $http.get('../data/quizzes.json').success(function (response) {
-        $scope.quizData  = response;
-    });
+testControllers.controller('userCtrl',['$scope','loadTestFactory' ,function ($scope , loadTestFactory) {
+            $scope.quizData  = loadTestFactory.getData();
 }]);
 
-testControllers.controller('testCtrl',['$scope','$http' ,function ($scope) {
+testControllers.controller('testCtrl',['$scope','loadTestFactory' ,function ($scope, loadTestFactory) {
+    $scope.answers= [];
+    $scope.testStatus = "pending";
+    $scope.quizId = null;
+    $scope.testTitle = "";
+    $scope.activequiz = [];
+    $scope.qTitle = "";
+    $scope.counter = 0;
+    $scope.options = [];
+
+    $scope.quizData2  = loadTestFactory.getData();
+    
+    /*start quiz*/
+    $scope.startQuiz = function () {
+        $scope.testStatus = "inProgress";
+        $scope.activequiz = loadTestFactory.getQuiz(0);
+        $scope.testTitle = $scope.activequiz.title;
+        $scope.qTitle = $scope.activequiz.questions[0].questionTitle;
+
+        $scope.options = $scope.activequiz.questions[0].answer;
+
+    };
+
+    /*Add answer to answers array*/
+    $scope.addAns = function (answer, questionIndex) {
+    };
+
+    $scope.saveAnswers = function (answers) {
+    };
+
+    $scope.getResult = function () {
+    }
+
+    $scope.endQuiz = function () {
+        $scope.testStatus = "inactive";
+    }
+
 
 }]);
--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,33 @@
 var userApp = angular.module('userApp', ['ngRoute', 'testControllers']);
 
+/*Factory that loads data from json file*/
+userApp.factory('loadTestFactory', function ($http) {
+    var quizData = [];
+
+    function loadData() {
+        $http.get('../data/quizzes.json').success(function (response) {
+            quizData  = response;
+        });
+    }
+    return{
+        getData: function () {
+            return quizData;
+        },
+        getQuiz: function (quizIndex) {
+            return quizData.quizzes[quizIndex];
+        },
+        loadData :loadData
+    }
+});
+
 userApp.config(['$routeProvider', function($routeProvider) {
     $routeProvider.
     when('/', {
+        resolve: {
+            load:function (loadTestFactory) {
+                return loadTestFactory.loadData(); // loads and saves data in factory
+            }
+        },
         templateUrl: 'partials/userMenu.html',
         controller: 'userCtrl'
     }).
