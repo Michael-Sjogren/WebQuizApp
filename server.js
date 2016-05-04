80451a0593af282acace2195f30b776a1829a09a
michael sjögren <mikel-carl@hotmail.com>
2016-05-04 14:13:05 +0200

    server.js file added and fixed connection to our db via mongoose

---
+++ server.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Michael Sjögren on 2016-05-04.
+ */
+var mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://admin1:A9c3aZ72@ds013222.mlab.com:13222/heroku_8z539qtl');
+var user = "admin1";
+var password = "A9ca@Z72";
+var dbHost = mongoose.connection;
+
+
+dbHost.on('error', console.error.bind(console, 'connection error:'));
+
+dbHost.once('open', function(){
+    
+    console.log("Connected to DB");
+
+});
+
+
