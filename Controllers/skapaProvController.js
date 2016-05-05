24949e803e058c3bf97e59e87ed57ce359e8a9ea
Gniweist <tsiewings@gmail.com>
2016-05-05 22:21:02 +0200

    Improved the Reset()

--- Controllers/skapaProvController.js
+++ Controllers/skapaProvController.js
@@ -43,7 +43,7 @@ adminApp.controller('skapaProvController', function($scope, $http) {
     };
 
     $scope.reset = function() {
-        $scope.adminProv = angular.copy($scope.master);
+        $scope.adminProv = "";
 
     };
     $scope.reset();
