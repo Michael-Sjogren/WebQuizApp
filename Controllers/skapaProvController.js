01aeb0fa6200f69d4f7642ac0b0f7fbc01354d25
Gniweist <tsiewings@gmail.com>
2016-05-06 12:06:43 +0200

    Tilldela prov till klass eller elev
    markup and functions in controller added

--- Controllers/skapaProvController.js
+++ Controllers/skapaProvController.js
@@ -22,6 +22,13 @@ adminApp.controller('skapaProvController', function($scope, $http) {
         {provetsNamn: 'Kravanalys 1'},
         {provetsNamn: '.Net'}
     ];
+    $scope.provTilldela = [
+        {provTilldel: 'IT-säkerhetsteknik'},
+        {provTilldel: 'Java SE'},
+        {provTilldel: 'Java EE'},
+        {provTilldel: 'Kravanalys 1'},
+        {provTilldel: '.Net'}
+    ];
 
     $scope.count = 1;
     $scope.counterFraga = function () {
@@ -63,6 +70,53 @@ adminApp.controller('skapaProvController', function($scope, $http) {
                     console.log(data);
                 }
             )
+    }
+
+    function InstantSearchController ($scope){
+
+        // The data model. These items would normally be requested via AJAX,
+        // but are hardcoded here for simplicity. See the next example for
+        // tips on using AJAX.
+
+        $scope.items = [
+            {
+                url: 'http://www.tutorialspoint.com/android/',
+                title: 'Android tutorials',
+                image: 'http://www.tutorialspoint.com/android/images/android-mini-logo.jpg'
+            },
+            {
+                url: 'http://www.tutorialspoint.com/angularjs/',
+                title: 'AngularJs Tutorials ',
+                image: 'http://www.tutorialspoint.com/angularjs/images/angularjs-mini-logo.jpg'
+            },
+            {
+                url: 'http://www.tutorialspoint.com/html5/',
+                title: 'HTML5 Tutorials',
+                image: 'http://www.tutorialspoint.com/html5/images/html5-mini-logo.jpg'
+            },
+            {
+                url: 'http://www.tutorialspoint.com/css/',
+                title: 'CSS Tutorials',
+                image: 'http://www.tutorialspoint.com/css/images/css-mini-logo.jpg'
+            },
+            {
+                url: 'http://www.tutorialspoint.com/java/',
+                title: 'Java Tutorials',
+                image: 'http://www.tutorialspoint.com/java/images/java-mini-logo.jpg'
+            },
+            {
+                url: 'http://www.tutorialspoint.com/joomla/',
+                title: 'Joomla Tutorials',
+                image: 'http://www.tutorialspoint.com/joomla/images/joomla-mini-logo.jpg'
+            },
+            {
+                url: 'http://www.tutorialspoint.com/html/',
+                title: 'HTML Tutorials ',
+                image: 'http://www.tutorialspoint.com/html/images/html-mini-logo.jpg'
+            }
+        ];
+
+
     }
 
     });
